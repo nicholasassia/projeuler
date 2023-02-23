358f7f5cb4adb8fc9d23ac63650d626bb3476c19
nicholasassia <56373579+nicholasassia@users.noreply.github.com>
2023-02-23 11:48:35 -0500

    Add files via upload
    quick one while bored

---
+++ euler2.py
@@ -0,0 +1,13 @@
+def fib():
+    a = 1 
+    b = 2
+    evenfib = 0
+    
+    while a <= 4000000:
+        if (a % 2) == 0:
+            evenfib += a
+        print(a)
+        a, b = b, a+b
+    print(f'sum of even vals: {evenfib}')
+        
+fib()
